dd2ba7b9bdc4217797a912cea085fbc917bafe04
MyViboon <bakpu11@gmail.com>
2022-04-26 23:30:10 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,16 +2,19 @@ const express = require('express');
 const chalk = require('chalk')
 const debug = require('debug')('app');
 const morgan = require('morgan');
+const path = require('path');
+
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, "/public/")));
 app.use(morgan('combined'));
-app.get("/", (req,res) =>{
+// app.get("/", (req,res) =>{
 
-    res.send("Hello Viboon Back Back");
+//     res.send("Hello Viboon Back Back");
 
-})
+// })
 
 app.listen(port, ()=>{
-    console.log("Listening on Port"+ chalk.blue(port));
+    debug("Listening on Port"+ chalk.blue(port));
 })
