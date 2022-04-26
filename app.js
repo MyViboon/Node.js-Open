3f0c27751fc743fb89315f2f1f01338aae808c25
MyViboon <bakpu11@gmail.com>
2022-04-26 23:48:01 +0700

    UpWeb

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, "/public/")));
+
 app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname, "/public/")));
 // app.get("/", (req,res) =>{
 
 //     res.send("Hello Viboon Back Back");
